33774ad4b81134db76fd2dbc0ff055b80443bb9b
debas <debas@DESKTOP-U88BJD6>
2021-09-05 12:48:46 +0600

    Add project files.

---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
