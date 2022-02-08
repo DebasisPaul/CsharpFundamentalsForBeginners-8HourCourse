b2a589842ff8194feff748766ea694fad8581f10
DEBASIS PAUL®™ (Zhýralý Pāvils) #BlackLivesStillMatter <DebasisPaul@users.noreply.github.com>
2022-02-08 16:29:52 +0600

    Update Syntax.cs

--- W3SchoolsC#Tutorials/Syntax.cs
+++ W3SchoolsC#Tutorials/Syntax.cs
@@ -6,12 +6,17 @@ using System;
 namespace HelloWorld
 
   // Namespace is use to organise your code and it is a container for classes & other namespaces.
+  // The Curly braces {} marks the begining and the end of a block of code.
 {
   class Program
+    
+    // Class is a container for data & methods, which brings funtionality to your program. Every lines of code that runs in c# must be inside in a class.
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");    
+      Console.WriteLine("Hello World!");   
+      // Console is a class of the System namespace, which has a WriteLine() method that is output/print text. In our example it will output "Hello World!",
+      // if u omit the using System, you would have to write System.Console.WriteLine() to print/output text. 
     }
   }
 }
