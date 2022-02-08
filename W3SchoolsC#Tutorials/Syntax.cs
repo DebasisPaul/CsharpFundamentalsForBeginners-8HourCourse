d8ed50165e78dafc44fa414df12e88b552582eed
DEBASIS PAUL®™ (Zhýralý Pāvils) #BlackLivesStillMatter <DebasisPaul@users.noreply.github.com>
2022-02-08 16:07:48 +0600

    Update Syntax.cs

--- W3SchoolsC#Tutorials/Syntax.cs
+++ W3SchoolsC#Tutorials/Syntax.cs
@@ -1,7 +1,11 @@
 
 using System;
 
+// Using System means that we can use classes from the system namespace.
+
 namespace HelloWorld
+
+  // Namespace is use to organise your code and it is a container for classes & other namespaces.
 {
   class Program
   {
