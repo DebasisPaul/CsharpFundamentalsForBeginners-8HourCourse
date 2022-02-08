33f1fde7963983ddef914583f31ac28583139d18
DEBASIS PAUL®™ (Zhýralý Pāvils) #BlackLivesStillMatter <DebasisPaul@users.noreply.github.com>
2022-02-08 16:36:21 +0600

    Update and rename Comments.md to Comments.cs

---
+++ W3SchoolsC#Tutorials/Comments.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // This is a Single Line comment
+      
+      /* The code below will print the words Hello World
+      to the screen, and it is amazing */
+      Console.WriteLine("Hello World!");    
+     }
+  }
+}
