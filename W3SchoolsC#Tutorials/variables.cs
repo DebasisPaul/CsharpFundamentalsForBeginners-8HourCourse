909c2e81d45993eaa3e90e2b86b7d4715a028600
DEBASIS PAUL®™ (Zhýralý Pāvils) #BlackLivesStillMatter <DebasisPaul@users.noreply.github.com>
2022-02-08 17:25:48 +0600

    Update variables.cs

--- W3SchoolsC#Tutorials/variables.cs
+++ W3SchoolsC#Tutorials/variables.cs
@@ -1,5 +1,4 @@
 /* 
-// String variable
 using System;
 namespace MyApplication
 {
@@ -7,8 +6,38 @@ namespace MyApplication
   {
     static void Main(string[] args)
     {
+    
+    // String variable
      string name = "John";
      Console.WriteLine(name);
      }
   }
 } */
+
+/* using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // int variable
+      //Create a variable called myNum of type int and assign it the value 15:
+      int myNum = 15 ;
+      
+      //declare a variable without assigning the value, and assign the value later:
+      int myNum;
+      myNum = 15;
+      
+      Console.WriteLine(myNum);
+    }
+  }
+} */
+
+// Rules for naming Variables
+// Names can contains letters, digits, Underscore charecter
+// Names must begin with a charecter.
+// Names should start with a lowercase letter and it can not contain whitespace
+// Names are case sensitive ("myVar" and "myvar" are different variables)
+// Reserved words (like c# keywords such as int or double ) cannot be used as names
