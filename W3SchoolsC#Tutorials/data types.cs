09e92575f0b14348dfb538b6c339df8a075e8c7e
DEBASIS PAUL®™ (Zhýralý Pāvils) #BlackLivesStillMatter <DebasisPaul@users.noreply.github.com>
2022-02-08 17:51:17 +0600

    Update data types.cs

--- W3SchoolsC#Tutorials/data types.cs
+++ W3SchoolsC#Tutorials/data types.cs
@@ -1,1 +1,22 @@
+using System;
 
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int myNum = 15;              // integer (Whole number)
+      double myDoubleNum = 5.99D;  // Floating point number
+      char myLetter = "D"'         // Charecter
+      bool myBool = true;          // boolean
+      string myText = "Hello"     // String       
+      
+      Console.WriteLine(myNum);
+      Console.WriteLine(myDoubleNum);
+      Console.WriteLine(myLetter);
+      Console.WriteLine(myBool);
+      Console.WriteLine(myText);
+      }
+   }
+}
