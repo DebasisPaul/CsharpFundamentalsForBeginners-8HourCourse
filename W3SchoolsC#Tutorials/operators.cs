73dd6325c4a40d5e6981dbdbcf23d0f27887862d
DEBASIS PAUL®™ (Zhýralý Pāvils) #BlackLivesStillMatter <DebasisPaul@users.noreply.github.com>
2022-02-09 16:02:18 +0600

    Update and rename operators.md to operators.cs

---
+++ W3SchoolsC#Tutorials/operators.cs
@@ -0,0 +1,140 @@
+// Operators are used to perform operartions on variables & values.
+
+/* using System;
+
+namespace MyApplication
+{
+  class Program
+    {
+      static void Main(string[] args)
+      {
+        int sum1 = 100 + 50;     // 150(100+50)
+        int sum2 = sum1 + 250;   // 400 (150+ 250)
+        int sum3 = sum2 +sum2;   // 800 (400+400)
+        
+        Console.WriteLine(sum1);
+        Console.WriteLine(sum2);
+        Console.WriteLine(sum3);
+        }
+      }
+  } */
+       
+// Arithmetic Operators are used to perform common methemetical operations
+// Addition, Subtraction, Multiplication, Division, Modulus, Increment, 
+
+// Decrement Operators
+/* using System;
+namespace myApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 100 + 50;
+      Console.WriteLine(x);
+    }
+  }
+} */
+
+// Increment Operators
+/* using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 5;
+      x++;
+      Console.WriteLine(x);  
+     }
+  }
+} */
+
+
+// Assignment Operators
+
+/* using System;
+namespace myApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 10;
+      Console.WriteLine(x)
+      }
+    }
+ } */
+
+// Division operators
+/* using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 12;
+      int y = 3;
+      Console.WriteLine(x / y);  
+     }
+  }
+} */
+
+// Multiplication Operators
+/* using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 5;
+      int y = 3;
+      Console.WriteLine(x * y);  
+     }
+  }
+} */
+
+// Subtraction operators
+
+/* using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 5;
+      int y = 3;
+      Console.WriteLine(x - y);  
+     }
+  }
+} */
+
+// Addition Operators
+
+/* using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 5;
+      int y = 3;
+      Console.WriteLine(x + y);  
+     }
+  }
+} */
+ 
+ 
+// Assignment Operators
+// Comparison Operators
+// Logical operators
