68f446b72e47ab220ca8ece367e6f2931830d75f
DEBASIS PAUL®™ (Zhýralý Pāvils) #BlackLivesStillMatter <DebasisPaul@users.noreply.github.com>
2022-02-10 17:53:03 +0600

    Update and rename arrays.md to arrays.cs

---
+++ W3SchoolsC#Tutorials/arrays.cs
@@ -0,0 +1,35 @@
+// Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.To declare an array, define the variable type with square brackets.
+
+// string[] cars;
+// string[] cars = {"Volvo"; "BMW"; "Ford"; "Mazda"};
+// int[] myNum = {10, 20, 30, 40};
+
+//Access the Elements of an Array
+/* using System;
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] cars = {"Volvo", "BMW", "Mazda", "Flord"};
+      Console.WriteLine(cars[0]);
+    }
+  }
+} */
+
+// Change An Array Element
+cars[0] = "Opel";
+/* using System;
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] cars = {"Volvo" , "Ford" , "Mazda" , "BMW"}
+      cars[0] = "Opel";
+      Console.WriteLine(cars[0]);
+    }
+  }
+} */
