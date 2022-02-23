31ca0c9d9a4347d1be382a15955db713a6fc6f30
DEBASIS PAUL®™ (Zhýralý Pāvils) #BlackLivesStillMatter <DebasisPaul@users.noreply.github.com>
2022-02-23 12:22:34 +0600

    Update and rename oop to oop.cs

---
+++ W3SchoolsC#Tutorials/oop.cs
@@ -0,0 +1,2 @@
+// OOP Stands for object oriented Programming.
+// Procedural programming is about writing procedures or methods that perform operations on the data, while object-oriented programming is about creating objects that contain both data and methods.
