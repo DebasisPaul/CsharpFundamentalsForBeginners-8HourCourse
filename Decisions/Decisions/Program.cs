ed733d2a850cd10773cb7b71712c97370f059ec0
debasispaul <idebasispaul@gmail.com>
2022-08-08 06:19:05 -0700

    Decision

---
+++ Decisions/Decisions/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Decisions
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            
+            // Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = "";
+
+            if (userValue == "1")
+        //    {
+                //string message = "You won a new car!";
+                //Console.WriteLine(message);
+
+                //string message = "You won a new car!";
+
+                message = "You won a new car!"; 
+
+         //   }
+            else if (userValue == "2")
+          //  {
+                //string message = "you won a new boat!";
+                //Console.WriteLine(message);
+
+                //string message = "You won a boat";
+
+                message = "You won a new boat!";
+          //  }
+            else if (userValue == "3")
+            //{
+                //string message = "you won a new cat!";
+                //Console.WriteLine(message);
+
+                //string message = "You won a new cat!";
+
+                message = "You won a new cat!";
+           // }
+            else
+            //{
+                //string message = "Sorry, We didn't Understand.";
+                //Console.WriteLine(message);
+
+                //string message = "Sorry, we didn't understand.";
+
+               // message = "Sorry, we didn't understand. ";
+                //message = message + "You Lose.";
+                //message += "You Lose.";
+           // }
+
+            Console.WriteLine(message);
+            Console.ReadLine();*/
+
+
+
+            // Conditional Operator
+
+            /*
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            //Console.Write("You won a ");
+            //Console.Write(message);
+            //Console.Write(".");
+
+
+           // Console.WriteLine("You won a {0}.", message);
+            Console.WriteLine("You entered: {0}, therefore you won a {1},", userValue, message);
+            Console.ReadLine();
+            */
+            
+        }
+    }
+}
