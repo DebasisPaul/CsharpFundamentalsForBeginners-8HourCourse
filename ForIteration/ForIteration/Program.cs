f86ffe5f6990a753348384107ab957dd1fdfd4c3
debasispaul <idebasispaul@gmail.com>
2022-08-08 21:13:47 -0700

    ForIteration

---
+++ ForIteration/ForIteration/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ForIteration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            for (int i = 0; i < 10; i++)
+            {
+               // Console.WriteLine(i);
+
+                if (i == 7)
+                {
+                    Console.WriteLine("Found seven!");
+                    break;
+                }
+            }
+
+
+            for (int myValue =0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);
+            }
+
+
+            
+            Console.ReadLine();
+        }
+    }
+}
